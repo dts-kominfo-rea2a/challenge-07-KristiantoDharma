c3a0c4f99b9c28d9638b07241f59c1b8f7e276e8
Kristianto <kristiantowicaksana@gmail.com>
2022-06-12 08:04:09 +0700

    upd promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emotion) => {
+  try {
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+    const data = dataIXX.concat(dataVGC);
+    data.map((item) => {
+      item.hasil === emotion && (totalEmotion += 1)
+      return totalEmotion;
+    })
+  } catch (err) {
+    return err
+  }
+};
 
 module.exports = {
   promiseOutput,
