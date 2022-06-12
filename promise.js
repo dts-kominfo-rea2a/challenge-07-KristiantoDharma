49b574813503abbc8dc1362c8ff383938e9a5148
Kristianto <kristiantowicaksana@gmail.com>
2022-06-12 08:10:13 +0700

    upd promise.js add let totalEmotion

--- promise.js
+++ promise.js
@@ -6,6 +6,8 @@ const promiseOutput = async(emotion) => {
     const dataIXX = await promiseTheaterIXX();
     const dataVGC = await promiseTheaterVGC();
     const data = dataIXX.concat(dataVGC);
+
+    let totalEmotion = 0
     data.map(item => item.hasil === emotion && (totalEmotion += 1))
     return totalEmotion;
   } catch (err) {
